bfef75c05d35cc712250b0930b9cec93566953e1
Arnaud Chassagne <acs@mikrosimage.eu>
2012-04-27 16:42:37 +0200

    license webservice

---
+++ src/octopus/dispatcher/webservice/licenses.py
@@ -0,0 +1,40 @@
+'''
+Created on Apr 27, 2012
+
+@author: acs
+'''
+
+from octopus.core.framework import ResourceNotFoundError, BaseResource, queue
+from tornado.httpclient import HTTPError
+    
+    
+class LicensesResource(BaseResource):
+    @queue
+    def get(self):
+        self.writeCallback(repr(self.dispatcher.licenceManager))
+        
+class LicenseResource(BaseResource):
+    @queue
+    def get(self, licenseName):
+        try:
+            lic = self.dispatcher.licenceManager.licences[licenseName]
+            licenseRepr = repr(lic) + " by {"
+            for rn in lic.currentUsingRenderNodes:
+                licenseRepr += rn.name + ","
+            licenseRepr += "}"
+            self.writeCallback(licenseRepr)
+        except KeyError:
+            raise ResourceNotFoundError
+    
+    @queue
+    def put(self, licenseName):
+        data = self.getBodyAsJSON()
+        try:
+            maxLic = data['maxlic']
+        except KeyError:
+            return HTTPError(404, "Missing entry : 'maxlic'")
+        else:
+            self.dispatcher.licenceManager.setMaxLicencesNumber(licenseName, maxLic)
+            self.writeCallback("OK")
+            
+            

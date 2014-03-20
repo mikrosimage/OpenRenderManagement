126ae00204611c292c4424e607018c7287c8e21b
samson-jerome <jsa@mikrosimage.eu>
2014-03-20 18:46:27 +0100

    Add useful params to test job

--- Puli/scripts/examples/postgraphexample0.py
+++ Puli/scripts/examples/postgraphexample0.py
@@ -20,7 +20,10 @@ def process_args():
     parser.add_option("-p", "--port",       action="store", dest="port",        type=int,   default=8004,           help="Specified a target port")
     parser.add_option("-x", "--execute",    action="store_true", dest="execute",                                    help="Override submit param and executes job locally")
     parser.add_option("-d", "--display",    action="store_true", dest="dump",                                       help="Print graph json representation before process")
-    # parser.add_option("-t", "--time",       action="store", dest="time",        type=int,   default=30,             help="Time spend in command (will be randomized)")
+
+    parser.add_option("--min",              action="store", dest="min",    type=int,   default=20,             help="")
+    parser.add_option("--max",              action="store", dest="max",    type=int,   default=50,             help="")
+    parser.add_option("--num",              action="store", dest="num",    type=int,   default=10,             help="")
     options, args = parser.parse_args()
     return options, args
 
@@ -37,8 +40,9 @@ if __name__ == '__main__':
     #
     # Create custom graph
     #
-    args =  { "args":"sleep `shuf -i 20-50 -n 1`", "start":1, "end":10, "packetSize":1 }
-    tags =  { "prod":"test", "shot":"test", "nbFrames":10 }
+    command = "sleep `shuf -i %d-%d -n 1`" % (options.min, options.max )
+    args =  { "args": command, "start":1, "end":options.num, "packetSize":1 }
+    tags =  { "prod":"test", "shot":"test", "nbFrames":options.num }
     runner = "puliclient.contrib.commandlinerunner.CommandLineRunner"
 
     simpleTask = Task( name=options.jobname, arguments=args, tags=tags, runner=runner )

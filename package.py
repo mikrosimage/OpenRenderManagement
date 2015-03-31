6d6c8bdd3ab48ba90303f2a610978e6b5c32c802
samson-jerome <jsa@mikrosimage.eu>
2015-03-31 12:10:40 +0200

    Push eval for runnertoolkit

--- package.py
+++ package.py
@@ -1,13 +1,13 @@
 name = 'puli'
-version = '1.7.12'
+version = 'eval'
 
 # If your tool depends on some other package(s)
 requires = [
     'rez-current',
-    'python-2.7.3',
-    'tornado-dev',
-    'requests-dev',
-    'psutil-dev'
+    'python-2.7',
+    'tornado-2.2.1',
+    'requests',
+    'psutil'
 ]
 
 

073c118d0624b5fc5167eb8db2e4a3404fcee6f8
Elisa Prana <pra@mikrosimage.eu>
2015-05-22 10:25:03 +0200

    Added sleep argument in jobTest.py

--- scripts/util/jobTest.py
+++ scripts/util/jobTest.py
@@ -6,7 +6,7 @@ from puliclient import Task, Graph
 
 
 # Create Folder Nodes (ie Task Groups)
-def createFolderNodes(parent, listGroup, nbTasks, nbCmds):
+def createFolderNodes(parent, listGroup, nbTasks, nbCmds, sleep):
     name = "FolderNode"
     tags = {"prod": "mikros_test", "nbFrames": 1}
 
@@ -16,17 +16,17 @@ def createFolderNodes(parent, listGroup, nbTasks, nbCmds):
             print "Create %s" % name
             taskGroup = parent.addNewTaskGroup(name=name, tags=tags)
             tmp_listGroup = listGroup[:]
-            createFolderNodes(taskGroup, tmp_listGroup, nbTasks, args.nbCmds)
+            createFolderNodes(taskGroup, tmp_listGroup, nbTasks, args.nbCmds, sleep)
     else:
         for inbTask in range(nbTasks):
-            createTaskNode(parent, inbTask, nbCmds)
+            createTaskNode(parent, inbTask, nbCmds, sleep)
 
 # Create TaskNode (ie Task)
-def createTaskNode(parent, nbTasks, nbCmds):
+def createTaskNode(parent, nbTasks, nbCmds, sleep):
     name = "TaskNode"
     print "Create %s" % name
 
-    cmd = "echo 'command %%MI_FRAME%%'"
+    cmd = "sleep %s " % sleep
     arguments = {"cmd": cmd, "start": 0, "end": nbCmds - 1}
     tags = {"prod": "mikros_test", "nbFrames": 1}
     task = parent.addNewTask(name=name, arguments=arguments, tags=tags)
@@ -51,6 +51,10 @@ parser.add_argument('server',
                    metavar='SERVER',
                    type=str,
                    help='Name of the server on which to submit job')
+parser.add_argument('--sleep',
+                   type=int,
+                   default=5,
+                   help='Sleep duration in s')
 args = parser.parse_args()
 
 # Create graph
@@ -64,7 +68,7 @@ print "NB_TASK = " + str(args.nbTasks)
 print "NB_CMD = " + str(args.nbCmds)
 print "SERVER = " + args.server
 
-createFolderNodes(puliGraph, args.nbGroups, args.nbTasks, args.nbCmds)
+createFolderNodes(puliGraph, args.nbGroups, args.nbTasks, args.nbCmds, args.sleep)
 
 puliGraph.submit(args.server, 8004)
 
